cd0edc54c35c71ae1c090ba84caa13121c524acd
GabrielM <gabrielmarcelino099@gmail.com>
2024-05-11 19:37:00 -0300

    fix url

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { StartQuiz } from "@/components/StartQuiz";
 import { QuizData } from "@/types";
 
 export const getData = async (): Promise<QuizData> => {
-  const response = await fetch("https://quiz-app-five-ochre.vercel.app/", {
+  const response = await fetch("http://localhost:3000/api/json", {
     cache: "no-cache",
   });
   const data: QuizData = await response.json();
